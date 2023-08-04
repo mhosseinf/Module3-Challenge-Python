69c25ad0506b80d26b515b3f7f5271efd2fa67f1
mhosseinf <mh30f@yahoo.com>
2023-08-04 12:45:04 +0800

    Greatest Decrease sorted

--- Results/PyBank-Outputs/Module 3 Challenge-Python.py
+++ Results/PyBank-Outputs/Module 3 Challenge-Python.py
@@ -6,6 +6,7 @@ previous_value = 0
 change_sum = 0
 count = 0
 greatest_Increase=0
+greatest_Decrease=0
 with open(file_path, 'r', newline='') as csvfile:
     csv_reader = csv.reader(csvfile)
     next(csv_reader)
@@ -27,6 +28,10 @@ with open(file_path, 'r', newline='') as csvfile:
             greatest_Increase = current_change
             greatest_Increase_month=row[0]
         
+        if current_change < greatest_Decrease:
+            greatest_Decrease = current_change
+            greatest_Decrease_month=row[0]
+
         previous_value = column_2_Value
 if count>0:
     average_change=change_sum/count
@@ -38,3 +43,4 @@ print("Total number of lines in the CSV file:", total_lines)
 print("Net total amount of Profit/Losses is:", sum_column_2)
 print("changes in Profit/Losses over the entire period:", average_change)
 print(f"Greatest Increase in Profits: {greatest_Increase_month} {greatest_Increase}")
+print(f"Greatest Decrease in Profits: {greatest_Decrease_month} {greatest_Decrease}")
